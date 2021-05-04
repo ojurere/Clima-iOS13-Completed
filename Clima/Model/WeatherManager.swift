fab80a1d92638405738bec717a4e666807dba80a
ojurere <58527680+ojurere@users.noreply.github.com>
2021-05-04 11:37:58 -0500

    good stuff

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -15,7 +15,7 @@ protocol WeatherManagerDelegate {
 }
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=7e6e2b6223caaef352572f4b1b557152&units=metric"
     
     var delegate: WeatherManagerDelegate?
     
@@ -30,19 +30,34 @@ struct WeatherManager {
     }
     
     func performRequest(with urlString: String) {
+        let headers = [
+            "x-rapidapi-key": "b37dc01042msh077b336df5f828ep1cd273jsn55c08db6651b",
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api-nba-v1.p.rapidapi.com/players/lastName/Harden")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
+            let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
                 if error != nil {
                     self.delegate?.didFailWithError(error: error!)
                     return
                 }
-                if let safeData = data {
-                    if let weather = self.parseJSON(safeData) {
-                        self.delegate?.didUpdateWeather(self, weather: weather)
-                    }
-                }
+//                if let safeData = data {
+//                    if let weather = self.parseJSON(safeData) {
+//                        self.delegate?.didUpdateWeather(self, weather: weather)
+//                    }
+//                }
+                let httpResponse = response as? HTTPURLResponse
+                let string = String(data: data!, encoding: .utf8)
+                print(string)
             }
+            
             task.resume()
         }
     }
